29dd1ab92cebb838e31a6ba6b1c881b3b2123688
Yudhir Bhattarai <yudhir_bhattarai@epam.com>
2023-04-19 20:05:56 -0400

    linting fix

--- task_app/urls.py
+++ task_app/urls.py
@@ -34,9 +34,10 @@ urlpatterns = [
     re_path('^', include(router.urls)),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$',
             schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    re_path(r'^swagger/$', schema_view.with_ui('swagger',
-                                               cache_timeout=0), name='schema-swagger-ui'),
-    re_path(r'^redoc/$', schema_view.with_ui('redoc',
-                                             cache_timeout=0), name='schema-redoc'),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0),
+            name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0),
+            name='schema-redoc'),
+    path('api-auth/',
+         include('rest_framework.urls', namespace='rest_framework'))
 ]
--- task_app/views.py
+++ task_app/views.py
@@ -1,5 +1,4 @@
 from rest_framework import viewsets
-# from rest_framework import permissions
 
 from .models import Project, Task, Team, User
 from .serializers import (
@@ -14,8 +13,10 @@ class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
     permission_classes = []
-    filterset_fields = ['id', 'title', 'body', 'due_date', 'status', 'creation', 'priority', 'story_point',
-                        'subtasks', 'project', 'creator', 'assigned_primary', 'assigned_secondary', 'parent_task']
+    filterset_fields = ['id', 'title', 'body', 'due_date', 'status', 'creation',
+                        'priority', 'story_point',
+                        'subtasks', 'project', 'creator', 'assigned_primary',
+                        'assigned_secondary', 'parent_task']
 
 
 class ProjectViewSet(viewsets.ModelViewSet):
@@ -28,8 +29,10 @@ class ProjectViewSet(viewsets.ModelViewSet):
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    filterset_fields = ['id', 'username', 'first_name', 'last_name', 'created_tasks',
-                        'assigned_tasks_primary', 'assigned_tasks_secondary', 'created_projects']
+    filterset_fields = ['id', 'username', 'first_name', 'last_name',
+                        'created_tasks',
+                        'assigned_tasks_primary', 'assigned_tasks_secondary',
+                        'created_projects']
     permission_classes = []
 
 
@@ -38,4 +41,3 @@ class TeamViewSet(viewsets.ModelViewSet):
     serializer_class = TeamSerializer
     filterset_fields = ['id', 'name']
     permission_classes = []
-    # permission_classes = [permissions.IsAuthenticated]
